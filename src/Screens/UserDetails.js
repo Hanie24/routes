b6defce948ce2f4dfc5cb212a3a62ddd037fdd3c
Hanie24 <sailor_121108@hotmail.com>
2023-06-14 23:44:14 -0600

    firebase

--- src/Screens/UserDetails.js
+++ src/Screens/UserDetails.js
@@ -4,12 +4,24 @@ import { Text, View } from 'react-native';
 import { stylesScreen } from './ScreenStyle';
 import ComponentButton from '../components/ComponentButton';
 
+import firebase from '../data/firebase';
+import { collection, getDocs, getFirestore } from '@firebase/firestore';
+
 export default function UserDetails({navigation}) {
+  
+  const queryGetUser = async () => {
+    const dataBase = getFirestore(firebase);
+    const data = await getDocs(collection(dataBase, 'users'));
+    data.forEach((doc) => {
+      console.table(doc.data());
+    })
+  }
+
+  queryGetUser();
+
   return (
     <View style={stylesScreen.container}>
-      <Text>About Us!</Text>
-      <ComponentButton title="Ir a atras" onPress={() => navigation.goBack()} />
-      <StatusBar style="auto" />
+      
     </View>
   );
 }
